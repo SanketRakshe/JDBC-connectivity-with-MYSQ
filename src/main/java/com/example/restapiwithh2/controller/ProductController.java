52f590b49193a38465d477655ec5c765688e37b3
SanketRakshe <sanketrakshe07@gmail.com>
2024-09-24 14:20:02 +0530

    FEAT: added put and delete mapping in controller

--- src/main/java/com/example/restapiwithh2/controller/ProductController.java
+++ src/main/java/com/example/restapiwithh2/controller/ProductController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +38,18 @@ public class ProductController {
 	@PostMapping
 	public Product addProduct(@RequestBody Product product) {
 		return productService.addProduct(product);
+	}
+	
+	@PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        Product updatedProduct = productService.updateProduct(id, product);
+        return updatedProduct != null ? ResponseEntity.ok(updatedProduct) : ResponseEntity.notFound().build();
+    }
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteProduct(@PathVariable  Long id) {
+		productService.deleteProduct(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 	
