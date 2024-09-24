61311a79f2a4aedcfc889c438bf27ef8051cba23
SanketRakshe <sanketrakshe07@gmail.com>
2024-09-24 14:08:03 +0530

    FEAT: added PostMapping in the controller

--- src/main/java/com/example/restapiwithh2/controller/ProductController.java
+++ src/main/java/com/example/restapiwithh2/controller/ProductController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +33,10 @@ public class ProductController {
         return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 	
+	@PostMapping
+	public Product addProduct(@RequestBody Product product) {
+		return productService.addProduct(product);
+	}
 	
 	
 	
